5df765bc75790c2ad223e385a476e5e08aa8e999
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2024-08-26 12:11:52 +0530

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	int t;
+	cin >> t;
+
+	while(t--){
+		int a,b;
+		char op;
+		cin >> a >> op >> b;
+		cout << a+b << endl;
+    }
+
+
+	return 0;
+}
