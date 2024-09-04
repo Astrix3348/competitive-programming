91fe5ad16b41f3e7b5000296ce21ebeb99c07568
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2024-09-04 11:34:31 +0530

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+	int t;
+	cin >> t;
+	
+	while(t--){
+		int x,y;
+		cin >> x >> y;
+		if(x>y){
+			cout << y << " " << x << endl;
+		}
+		else if(x==y){
+			cout << x << " " << y << endl;
+		}
+		else{
+			cout << x << " " << y << endl;
+		}
+	}
+
+	return 0;
+}
