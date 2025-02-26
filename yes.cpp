50209ece7fb2c746e37976ddc7551903a318197a
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-02-26 18:17:27 +0530

    Add files via upload

---
+++ yes.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<cctype>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	while(n--){
+		string s;
+		cin >> s;
+		transform(s.begin(), s.end(), s.begin(), ::tolower);
+		if(s == "yes"){
+			cout << "YES" << endl;
+		}
+		else{
+			cout << "NO" << endl;
+		}
+	}
+
+
+
+	return 0;
+}
