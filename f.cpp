363a59fabac4468a05bb307e83a39bdfb4efcb24
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-08-04 19:24:35 +0530

    Add files via upload

--- f.cpp
+++ f.cpp
@@ -1,14 +1,20 @@
 #include<iostream>
+#include<string.h>
 using namespace std;
 
 int main(){
-	int w;
-	cin >> w;
-	if(w%2==0 && w > 2){
-		cout << "YES" << endl;
+	int n, t=0;
+	string s;
+	cin >> n;
+	while(n--){
+		cin >> s;
+		if(s=="X++" || s=="++X"){
+			t++;
+		}
+		else{
+			t--;
+		}
 	}
-	else{
-		cout << "NO" << endl;
-	}
-  return 0;
+	cout << t << endl;
+	return 0;
 }
